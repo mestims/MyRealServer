9015f73be2fbdb17f54319d53254af3d9f0c8a84
msambiase <msambiase@uolinc.com>
2019-03-15 14:55:16 -0300

    refactoring EurekaServer with kotlin

---
+++ EurekaServer/src/main/kotlin/br/com/server/eureka/EurekaApplication.kt
@@ -0,0 +1,13 @@
+package br.com.server.eureka
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
+
+@SpringBootApplication
+@EnableEurekaServer
+class EurekaApplication
+
+fun main() {
+    runApplication<EurekaApplication>()
+}
