682be23681571c0c41618be766dbd5566d3d2298
msambiase <msambiase@uolinc.com>
2019-03-15 10:02:34 -0300

    refactoring ImageService Application

--- ImageService/src/main/kotlin/br/com/server/image/ImageApplication.kt
+++ ImageService/src/main/kotlin/br/com/server/image/ImageApplication.kt
@@ -8,7 +8,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 @EnableEurekaClient
 class ImageApplication
 
-fun main(args: Array<String>) {
-    runApplication<ImageApplication>(*args)
+fun main() {
+    runApplication<ImageApplication>()
 }
 
